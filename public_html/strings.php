6ce284a0284bd49e7bc43786f7df7083ca089d20
Heinz K <heka@techcollege.dk>
2022-06-14 08:37:22 +0200

    Update af strengeleg

--- public_html/strings.php
+++ public_html/strings.php
@@ -1,11 +1,11 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/assets/incl/functions.php";
 
-$strPageTitle = "Velkommen til mit portfolio";
+$strPageTitle = "Strengeleg";
 $strClass = "myHeader";
-echo "<h1 class='$strClass'>$strPageTitle</h1>";
+echo "<h1 class='$strClass'>$strPageTitle</h1>\n<hr />";
 
-$txt1 = "Til Bo Nicolajsen\n
+$str1 = "Til Bo Nicolajsen\n
 		Vi skriver fordi der endnu er penge på din konto og den er blevet spærret. 
 		Grundet vi har skiftet platform bedes du oprette din konto på ny med email 
 		adressen: bo@someplace.dk - Efter oprettelse vil dine penge vente på din 
@@ -13,29 +13,79 @@ $txt1 = "Til Bo Nicolajsen\n
 		Beløb tilgængeligt opgjort pr. : 21.405,52 kr.\n\n
 		Venlig hilsen Tina";
 
-$txt2 = "Hel Tina\n
+$str2 = "Hej Tina\n
 		Da jeg er ufattelig rig, og derfor ikke har brug for pengene. 
 		Ser jeg gerne at i donere alle pengene til Dyrenes beskyttelse. 
 		Under navnet \"GeorgGiraf\".\n\n
 		Venlig hilsen Bo";
 
-$arrLetters = str_split($txt1);
-$i = 0;
-foreach($arrLetters as $key => $value) {
-	// Position for 7'ende e
-	if($value === "e") {
-		$i++;
-		if($i === 7) {
-			echo "<p>Opgave 5: $key</p>";
-		}
- 	}
+//$strTest1 = "abcdef";
+//$strTest2 = "defghi";
 
-	if($value === "E" && !isset($fix)) {
-		echo "<p>Opgave 6: $key</p>";
-		$fix = 1;
+echo showResult(2, strtoupper($str1));
+echo showResult(3, strtolower($str1));
+echo showResult(4, str_replace("email", "bob@someplace.dk", $str1));
+echo showResult(5, countPosition($str1, "e", 7));
+echo showResult(6, countPosition($str1, "E"));
+echo showResult(7, str_replace("rig", "fattig", $str2));
+echo showResult(8, str_replace("ikke", "", $str2));
+echo showResult(9, matchLetters($str1, $str2));
+echo showResult(10, shuffleChars($str1));
+
+/**
+ * Funktion der fjerner alle whitespaces og symboler 
+ * og splitter strengen op i et array
+ */
+function strToArray($str, $removeSpaces = false) {
+	$str = ($removeSpaces) ? preg_replace('/[^A-Za-z0-9]/', '', $str) : $str;
+	return str_split($str);
+}
+
+/**
+ * Funktion til at tæller en bestemt karakters placering i 
+ * den samlede streng.
+ */
+function countPosition($str, $char, $num = 1) {
+	$arrChars = strToArray($str); 
+	$i = 0;
+	foreach($arrChars as $key => $value) {
+		if($value === $char) {
+			$i++;
+			if($i === $num) {
+				return $key;
+			}
+		}
 	}
 }
 
-echo "<p>Opgave 7: " . str_replace("rig", "fattig", $txt2). "</p>";
-echo "<p>Opgave 8: " . str_replace("ikke", "", $txt2). "</p>";
+/**
+ * Matcher karakterværdier i to forskellige strenge
+ * Fjerner whitespaces og dubletter og splitter strenge i arrays
+ * Returnerer antal af matches
+ */
+function matchLetters($str1, $str2) {
+	$arrChars1 = strToArray($str1, true);
+	$arrChars2 = strToArray($str2, true);
+	$arr1 = array_unique($arrChars1);
+	$arr2 = array_unique($arrChars2);
+	$result_array = array_intersect($arrChars1, $arrChars2);
+	return count($result_array);
+}
+
+/**
+ * Blander alle bogstaver tilfældigt i en tekst
+ */
+function shuffleChars($str) {
+	$arrChars = strToArray($str);
+	shuffle($arrChars);
+	return implode('', $arrChars);
+}
+
+/**
+ * Viser resultat med opgave nummer
+ */
+function showResult($id, $result) {
+	return "<div>\n<h2>Opgave nr. $id</h2>\n 
+				<p><b>Resultat:</b><br>$result</p>\n<hr></div>";
+}
 ?>
