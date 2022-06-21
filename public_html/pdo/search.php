72d9f9d1517eb5459078cee6ace3ea6f262826d8
Heinz K <heka@techcollege.dk>
2022-06-21 12:52:05 +0200

    Search Example Update

--- public_html/pdo/search.php
+++ public_html/pdo/search.php
@@ -2,12 +2,15 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/assets/incl/init.php";
 Helpers::Header("Velkommen");
 
+// Henter keyword fra GET parametre
 $keyword = isset($_GET['keyword']) && !empty($_GET['keyword']) ? $_GET['keyword'] : null;
 
+// Sætter array med keyword og wildcards
 $params = array(
 	"keyword" => array("%$keyword%", PDO::PARAM_STR)
 );
 
+// SQL Statement
 $sql = "SELECT s.title, a.name 
 		FROM song s 
 		JOIN artist a 
