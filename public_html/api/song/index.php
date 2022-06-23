d15cb5d43342d70a9b7957eed198885221988dfd
Heinz K <heka@techcollege.dk>
2022-06-23 09:31:33 +0200

    Route Update - file renaming, guide updates

--- public_html/api/song/index.php
+++ public_html/api/song/index.php
@@ -1,10 +1,8 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/assets/incl/init.php";
 
-Router::add('/api/song/', function() {
-	$object = new Song;
-	$response = $object->list();
-	echo Helpers::jsonParser($response);
+Route::add('/api/song/', function() {
+	echo 1234;
 });
 
-Router::run('/');
+Route::run('/');
